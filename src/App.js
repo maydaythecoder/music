6e82767d72fa232ddf3eab87ad6b28a8a472d6fb
mayday <maydaythecoder@gmail.com>
2023-06-18 15:44:33 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,33 @@ import './App.css';
 import './components/ShazamSearch';
 import './components/genres';
 import './components/songs'
+import axios from 'axios'
+
+
+const axios = require('axios');
+
+const options = {
+  method: 'GET',
+  url: 'https://shazam.p.rapidapi.com/shazam-events/list',
+  params: {
+    artistId: '73406786',
+    l: 'en-US',
+    from: '2022-12-31',
+    limit: '50',
+    offset: '0'
+  },
+  headers: {
+    'X-RapidAPI-Key': '4fa00a0351mshfded974940fc926p1e970ajsnfe19efc5fbb3',
+    'X-RapidAPI-Host': 'shazam.p.rapidapi.com'
+  }
+};
+
+try {
+	const response = await axios.request(options);
+	console.log(response.data);
+} catch (error) {
+	console.error(error);
+}
 
 
 function App() {
--- src/components/ShazamSearch.js
+++ src/components/ShazamSearch.js
@@ -1,11 +1,12 @@
 import React from 'react'
 
+
 const ShazamSearch = () => {
 
-    
+
   return (
     <div>
-      
+
     </div>
   )
 }
